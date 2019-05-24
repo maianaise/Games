df1de0e99ed0a570a7e902d6bb582bd1a71f986d
maianaise <36210558+maianaise@users.noreply.github.com>
2019-05-24 11:27:09 -0400

    Add files via upload
    Updated number guesser

---
+++ NumberClicker.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class NumberClicker : MonoBehaviour
+{
+    [SerializeField] int maximum;
+    [SerializeField] int minimum;
+    [SerializeField] Text guessText;
+
+    int guess;
+
+    void Start()
+    {
+        startGame();
+    }
+
+    public void onPressHigher()
+    {
+        minimum = guess;
+        guess = (minimum + maximum) / 2;
+        guessText.text = guess.ToString();
+    }
+    public void onPressLower()
+    {
+        maximum = guess;
+        guess = (minimum + maximum) / 2;
+        guessText.text = guess.ToString();
+    }
+    public void startGame()
+    {
+        guess = Random.Range(minimum, maximum);
+        guessText.text = guess.ToString();
+    }
+    
+}
---
+++ SceneLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void loadNextScene()
+    {
+        int currrentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currrentSceneIndex + 1);
+    }
+
+    public void loadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
